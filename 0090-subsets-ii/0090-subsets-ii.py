801a8a49029fa138b9b0a15c50aef683e5173269
Shreeraj <55228842+shreeraj2698@users.noreply.github.com>
2022-10-27 16:40:44 -0700

    Time: 73 ms (5.89%), Space: 13.7 MB (48.84%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -5,31 +5,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # ans = list([])
-        # dup_free_set = []
-        # def all_possible(n):
-        #     if(n<0):
-        #         return;
-        #     for i in itertools.combinations(nums, n):
-        #         if sorted(list(i)) not in dup_free_set:
-        #             ans.append(i)
-        #             dup_free_set.append(sorted(i))
-        #     all_possible(n-1)
-        # all_possible(len(nums))
-        # return list(set(ans))
+        ans = list([])
+        dup_free_set = []
+        def all_possible(n):
+            if(n<0):
+                return;
+            for i in itertools.combinations(nums, n):
+                if sorted(list(i)) not in dup_free_set:
+                    ans.append(i)
+                    dup_free_set.append(sorted(i))
+            all_possible(n-1)
+        all_possible(len(nums))
+        return list(set(ans))
         
-        ans = []
-        currSet = []
-        def addSubset(currSet, nums, index):
-            if(sorted(currSet) not in ans):
-                ans.append(sorted(copy.deepcopy(currSet)))
-            for i in range(index, len(nums)):
-                if i != index and nums[i] == nums[i-1]:
-                    continue
-                currSet.append(nums[i])
-                addSubset(currSet, nums, i+1)
-                currSet.pop()
-        sorted(nums)
-        addSubset(currSet, nums, 0)
-        return sorted(ans);
+        #******************backtracking************************
+        # ans = []
+        # currSet = []
+        # def addSubset(currSet, nums, index):
+        #     if(sorted(currSet) not in ans):
+        #         ans.append(sorted(copy.deepcopy(currSet)))
+        #     for i in range(index, len(nums)):
+        #         if i != index and nums[i] == nums[i-1]:
+        #             continue
+        #         currSet.append(nums[i])
+        #         addSubset(currSet, nums, i+1)
+        #         currSet.pop()
+        # sorted(nums)
+        # addSubset(currSet, nums, 0)
+        # return sorted(ans);
             
